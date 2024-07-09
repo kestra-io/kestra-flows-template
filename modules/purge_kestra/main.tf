4c57b9f9f651532a7f149999f594c00799e6e601
Antoine Balliet <antoine.balliet@gorgias.com>
2024-07-10 01:59:28 +0200

    feat: create purge_kestra tf module

---
+++ modules/purge_kestra/main.tf
@@ -0,0 +1,16 @@
+module "kestra_purge_db" {
+  source                   = "../postgres_query"
+  flow_id                  = var.flow_id
+  namespace                = var.namespace
+  description              = <<-EOT
+  Flow to purge Kestra logs from database.
+  Delete all logs lines older than ${var.retention_months} month(s).
+  It just remove logs from UI, executions are still visible.
+  EOT
+  postgres_user            = "kestra"
+  postgres_database        = "kestra"
+  postgres_password_secret = var.postgres_password_secret
+  postgres_host            = var.postgres_host
+  sql_query                = "DELETE FROM public.logs WHERE timestamp < NOW() - INTERVAL '${var.retention_months} month'"
+  trigger                  = var.trigger
+}
