9f41390e0caf3960c56691a5d5d1bbeee3f6830f
Antoine Balliet <antoine.balliet@gorgias.com>
2024-07-10 01:34:47 +0200

    feat: create dbt_run tf module

---
+++ modules/dbt_run/variables.tf
@@ -0,0 +1,49 @@
+variable "flow_id" {
+  type        = string
+  description = "Flow ID for the dbt run flow"
+}
+
+variable "description" {
+  type        = string
+  description = "Description of the flow"
+}
+
+variable "namespace" {
+  type        = string
+  default     = "blueprint"
+  description = "Namespace of the flow"
+}
+
+variable "labels" {
+  type        = map(string)
+  description = "Labels to apply to the flow"
+  default     = {}
+}
+
+variable "priority" {
+  type        = string
+  default     = null
+  description = "Priority tag to apply to the flow"
+}
+
+variable "trigger" {
+  type        = string
+  description = "String containing triggers sections of the flow"
+  default     = ""
+}
+
+variable "github_repo_url" {
+  type        = string
+  description = "Github repository to clone"
+}
+
+variable "dbt_commands" {
+  description = "dbt commands to run"
+  type        = list(string)
+}
+
+variable "dbt_max_retry_attempt" {
+  type        = number
+  description = "Number of retries for dbt run performed before the system stops retrying."
+  default     = 1
+}
