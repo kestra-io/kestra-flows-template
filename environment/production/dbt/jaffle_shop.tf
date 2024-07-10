001f5d7f0297d4b57d7e545efc0117e114883345
Antoine Balliet <antoine.balliet@gorgias.com>
2024-07-10 04:05:12 +0200

    chore: rename dbt file

--- environment/production/dbt/jaffle_shop.tf
+++ environment/production/dbt/jaffle_shop.tf
@@ -5,16 +5,16 @@ locals {
 
 ######### Jaffle Shop Classic Orders #########
 
-module "trigger_dbt_run_jaffle_shop_classic_orders" {
+module "trigger_dbt_run_jaffle_shop_orders" {
   source             = "../../../modules/trigger_cron"
   cron_name          = "trigger_dbt_jaffle_shope_orders_everyday_at_midnight"
   cron_expression    = "0 0 * * *" # Run every day at midnight
   late_maximum_delay = "PT1H"
 }
 
-module "dbt_run_jaffle_shop_classic_orders" {
+module "dbt_run_jaffle_shop_orders" {
   source          = "../../../modules/dbt_run"
-  flow_id         = "dbt_run_jaffle_shop_classic_orders"
+  flow_id         = "dbt_run_jaffle_shop_orders"
   description     = "Run orders model from dbt project jaffle-shop-classic"
   namespace       = local.namespace
   priority        = "high"
@@ -25,18 +25,18 @@ module "dbt_run_jaffle_shop_classic_orders" {
     "dbt run -s +orders",
   ]
   dbt_profile_name = local.dbt_profile_name
-  trigger          = module.trigger_dbt_run_jaffle_shop_classic_orders.trigger_content
+  trigger          = module.trigger_dbt_run_jaffle_shop_orders.trigger_content
 }
 
 ######### Jaffle Shop Classic Customers #########
 
-module "trigger_dbt_run_jaffle_shop_classic_customers" {
+module "trigger_dbt_run_jaffle_shop_customers" {
   source = "../../../modules/trigger_cron_hourly_random"
 }
 
-module "dbt_run_jaffle_shop_classic_customers" {
+module "dbt_run_jaffle_shop_customers" {
   source          = "../../../modules/dbt_run"
-  flow_id         = "dbt_run_jaffle_shop_classic_customers"
+  flow_id         = "dbt_run_jaffle_shop_customers"
   description     = "Run customers model from dbt project jaffle-shop-classic"
   namespace       = local.namespace
   priority        = "high"
@@ -47,5 +47,5 @@ module "dbt_run_jaffle_shop_classic_customers" {
     "dbt run -s +customers",
   ]
   dbt_profile_name = "jaffle_shop"
-  trigger          = module.trigger_dbt_run_jaffle_shop_classic_customers.trigger_content
+  trigger          = module.trigger_dbt_run_jaffle_shop_customers.trigger_content
 }
