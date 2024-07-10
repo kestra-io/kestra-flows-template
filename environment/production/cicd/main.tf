179ca335cebc34792192db59699d94e67df61562
Antoine Balliet <antoine.balliet@gorgias.com>
2024-07-10 03:35:09 +0200

    fix cicd flow

--- environment/production/cicd/main.tf
+++ environment/production/cicd/main.tf
@@ -1,5 +1,5 @@
 resource "kestra_flow" "kestra_tf_apply_main" {
   flow_id   = "kestra_tf_apply_main"
   namespace = "prod.cicd"
-  content   = file("${path.module}/tf_apply_kestra_tf.yml")
+  content   = file("${path.module}/terraform_apply_flows.yml")
 }
