23c84dd41b2fe544c70e792fd543e4b436c2acee
Antoine Balliet <antoine.balliet@gorgias.com>
2024-09-27 18:42:59 +0200

    fixes

--- environment/production/automation/etl_python/main.tf
+++ environment/production/automation/etl_python/main.tf
@@ -1,15 +1,7 @@
-resource "kestra_namespace_file" "default" {
-  namespace = "prod.automation"
-  filename  = "transform.py"
-  content   = file("${path.module}/transform.py")
-}
-
 resource "kestra_flow" "default" {
   flow_id   = "etl_python"
   namespace = "prod.automation"
   content = join("", [
-    templatefile("${path.module}/tasks.yml", {
-      python-file-uri = kestra_namespace_file.default.id
-    })
+    templatefile("${path.module}/tasks.yml", {})
   ])
 }
