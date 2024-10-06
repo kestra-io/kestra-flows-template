8b317a78562d70fcf094a70eca30f110bd023b90
Antoine Balliet <antoine.balliet@gorgias.com>
2024-10-06 22:12:16 +0200

    chore: use new main default branch

--- environment/production/dbt/kestra_dbt_example.tf
+++ environment/production/dbt/kestra_dbt_example.tf
@@ -16,10 +16,10 @@ module "dbt_run_kestra_example" {
   description     = "Run Kestra dbt example project"
   namespace       = local.namespace_kestra
   priority        = "low"
-  github_repo_url = "https://github.com/kestra-io/dbt-example"
-  git_branch      = "master"
+  github_repo_url = "https://github.com/kestra-io/dbt-example.git"
+  git_branch      = "main"
   dbt_commands = [
-    "dbt deps",
+    "dbt seed",
     "dbt build",
   ]
   dbt_profile_name = local.dbt_profile_kestra
